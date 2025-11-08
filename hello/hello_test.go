248d001e8ad0770292682e3f99a24638545f5c4d
meleu <meleu.dev@gmail.com>
2025-11-08 18:39:34 -0300

    Add multilingual support to Hello function

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,13 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		actual := Hello("meleu")
-		expected := "Hello, meleu"
+		actual := Hello("John", "")
+		expected := "Hello, John"
 		assertCorrectMessage(t, actual, expected)
 	})
+
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		actual := Hello("")
+		actual := Hello("", "")
 		expected := "Hello, World"
+		assertCorrectMessage(t, actual, expected)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		actual := Hello("Juan", "Spanish")
+		expected := "Hola, Juan"
+		assertCorrectMessage(t, actual, expected)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		actual := Hello("Jean", "French")
+		expected := "Bonjour, Jean"
 		assertCorrectMessage(t, actual, expected)
 	})
 }
