5b4f226272885855e04daf3504a30092515becaf
meleu <meleu.dev@gmail.com>
2025-11-08 18:17:34 -0300

    Handle empty string in Hello function

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	actual := Hello("meleu")
-	expected := "Hello, meleu"
+	t.Run("saying hello to people", func(t *testing.T) {
+		actual := Hello("meleu")
+		expected := "Hello, meleu"
+		assertCorrectMessage(t, actual, expected)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		actual := Hello("")
+		expected := "Hello, World"
+		assertCorrectMessage(t, actual, expected)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, actual, expected string) {
+	t.Helper()
 	if actual != expected {
-		t.Errorf("actual '%s', expected '%s'", actual, expected)
+		t.Errorf("\n  actual: %q\nexpected: %q", actual, expected)
 	}
 }
