141c7f5f374a62a12480cf1971eb02ebc8227a6a
meleu <meleu.dev@gmail.com>
2025-11-09 11:55:51 -0300

    Add SumAll and SumAllTails functions and tests

--- with-tests/arrays/sum.go
+++ with-tests/arrays/sum.go
@@ -9,3 +9,35 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	// lengthOfNumbers := len(numbersToSum)
+	// sums := make([]int, lengthOfNumbers)
+	//
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
--- with-tests/arrays/sum_test.go
+++ with-tests/arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -20,4 +23,34 @@ func assertEqualSum(t testing.TB, actual, expected int, givenNumbers []int) {
 			actual, expected, givenNumbers,
 		)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+	actual := SumAll([]int{1, 2}, []int{0, 1, 9})
+	expected := []int{3, 10}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("\n  actual: %v\nexpected: %v", actual, expected)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	assertEqualSums := func(t testing.TB, actual, expected []int) {
+		t.Helper()
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("\n  actual: %v\nexpected: %v", actual, expected)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		actual := SumAllTails([]int{1, 2}, []int{0, 1, 9})
+		expected := []int{2, 10}
+		assertEqualSums(t, actual, expected)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		actual := SumAllTails([]int{}, []int{3, 4, 5})
+		expected := []int{0, 9}
+		assertEqualSums(t, actual, expected)
+	})
 }
