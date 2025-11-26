faf37d028138822f34f00eb48b745f617392ff38
meleu <meleu.dev@gmail.com>
2025-11-26 20:50:52 -0300

    A line counter with options

--- the-power-of-go-tools/02-line-counter/count.go
+++ the-power-of-go-tools/02-line-counter/count.go
@@ -2,30 +2,66 @@ package count
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"os"
 )
 
 type counter struct {
-	Input io.Reader
+	input  io.Reader
+	output io.Writer
 }
 
-func NewCounter() *counter {
-	return &counter{
-		Input: os.Stdin,
+type option func(*counter) error
+
+func Main() {
+	c, err := NewCounter()
+	if err != nil {
+		panic(err)
 	}
+	fmt.Fprintln(c.output, c.Lines())
 }
 
 func (c *counter) Lines() int {
 	lines := 0
-	input := bufio.NewScanner(c.Input)
+	input := bufio.NewScanner(c.input)
 	for input.Scan() {
 		lines++
 	}
 	return lines
 }
 
-func Main() {
-	fmt.Println(NewCounter().Lines())
+func WithInput(input io.Reader) option {
+	return func(c *counter) error {
+		if input == nil {
+			return errors.New("nil input reader")
+		}
+		c.input = input
+		return nil
+	}
+}
+
+func WithOutput(output io.Writer) option {
+	return func(c *counter) error {
+		if output == nil {
+			return errors.New("nil output reader")
+		}
+		c.output = output
+		return nil
+	}
+}
+
+func NewCounter(opts ...option) (*counter, error) {
+	c := &counter{
+		input:  os.Stdin,
+		output: os.Stdout,
+	}
+	for _, opt := range opts {
+		err := opt(c)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return c, nil
 }
--- the-power-of-go-tools/02-line-counter/count_test.go
+++ the-power-of-go-tools/02-line-counter/count_test.go
@@ -7,13 +7,30 @@ import (
 	"github.com/meleu/count"
 )
 
-func TestCountReturnsNumberOfLines(t *testing.T) {
-	t.Parallel()
-	c := count.NewCounter()
-	c.Input = bytes.NewBufferString("1\n2\n3")
-	got := c.Lines()
-	want := 3
+// func TestCountReturnsNumberOfLines(t *testing.T) {
+// 	t.Parallel()
+// 	c := count.NewCounter()
+// 	c.Input = bytes.NewBufferString("1\n2\n3")
+// 	got := c.Lines()
+// 	want := 3
+//
+// 	if want != got {
+// 		t.Errorf("want %d, got %d", want, got)
+// 	}
+// }
 
+func TestLinesCountsLinesInInput(t *testing.T) {
+	t.Parallel()
+	inputBuf := bytes.NewBufferString("1\n2\n3")
+	c, err := count.NewCounter(
+		count.WithInput(inputBuf),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := 3
+	got := c.Lines()
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
